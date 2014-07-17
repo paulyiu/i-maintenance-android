32247626abbd668a9e07807642ecbbe5b6cf6a55
Paul Yiu <paul@metis.hk>
2014-07-17 17:58:16 +0800

    init g+ sign in

--- src/vtc/project/i_maintenance/MainActivity.java
+++ src/vtc/project/i_maintenance/MainActivity.java
@@ -1,18 +1,262 @@
 package vtc.project.i_maintenance;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.plus.People.LoadPeopleResult;
+import com.google.android.gms.plus.People;
+import com.google.android.gms.plus.Plus.PlusOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.plus.Plus;
+import com.google.android.gms.plus.PlusClient;
+import com.google.android.gms.plus.model.people.PersonBuffer;
+
+public class MainActivity extends Activity implements OnClickListener,
+OnConnectionFailedListener, ConnectionCallbacks, ResultCallback<People.LoadPeopleResult>  {
+
+    private TextView mSignInStatus;
+    private SignInButton mSignInButton;
+    private View mSignOutButton;
+    private View mRevokeAccessButton;
+
+
+	  /* Request code used to invoke sign in user interactions. */
+	  private static final int RC_SIGN_IN = 0;
+
+	    private static final int DIALOG_GET_GOOGLE_PLAY_SERVICES = 1;
+
+	    private static final int REQUEST_CODE_SIGN_IN = 1;
+	    private static final int REQUEST_CODE_GET_GOOGLE_PLAY_SERVICES = 2;
+		private static final String TAG = "vtc-project";
+
+
+	    
+	  /* Client used to interact with Google APIs. */
+	  private GoogleApiClient mGoogleApiClient;
+
+	  /* A flag indicating that a PendingIntent is in progress and prevents
+	   * us from starting further intents.
+	   */
+	  private boolean mIntentInProgress;
+	
+	  /* Track whether the sign-in button has been clicked so that we know to resolve
+	   * all issues preventing sign-in without waiting.
+	   */
+	  private boolean mSignInClicked;
+
+	  /* Store the connection result from onConnectionFailed callbacks so that we can
+	   * resolve them when the user clicks sign-in.
+	   */
+	  private ConnectionResult mConnectionResult;
+
+	  /* A helper method to resolve the current ConnectionResult error. */
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		PlusOptions mPlusApiOptions = PlusOptions.builder().build();
+	    mGoogleApiClient = new GoogleApiClient.Builder(this)
+        .addConnectionCallbacks(this)
+        .addOnConnectionFailedListener(this)
+        .addApi(Plus.API, mPlusApiOptions)
+        .addScope(Plus.SCOPE_PLUS_LOGIN)
+        .build();
+	    
+        mSignInStatus = (TextView) findViewById(R.id.sign_in_status);
+        mSignInButton = (SignInButton) findViewById(R.id.sign_in_button);
+        mSignInButton.setOnClickListener(this);
+        mSignOutButton = findViewById(R.id.sign_out_button);
+        mSignOutButton.setOnClickListener(this);
+        mRevokeAccessButton = findViewById(R.id.revoke_access_button);
+        mRevokeAccessButton.setOnClickListener(this);
 	}
+	
+	  protected void onStart() {
+		    super.onStart();
+		    mGoogleApiClient.connect();
 
+		  }
+	
+		  protected void onStop() {
+		    super.onStop();
+	
+		    if (mGoogleApiClient.isConnected()) {
+		      mGoogleApiClient.disconnect();
+		    }
+		  }
+		  
+		  public void onConnected(Bundle connectionHint) {
+			  mSignInClicked = false;
+			  Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+			  Plus.PeopleApi.loadVisible(mGoogleApiClient, null).setResultCallback(this);
+		        updateButtons(true /* isSignedIn */);
+			}
+		  
+		    public void onDisconnected() {
+		        mSignInStatus.setText(R.string.loading_status);
+		       // mPlusClient.connect();
+		        updateButtons(false /* isSignedIn */);
+		    }
+		    
+			  public void onConnectionFailed(ConnectionResult result) {
+				    if (!mIntentInProgress) {
+				      // Store the ConnectionResult so that we can use it later when the user clicks
+				      // 'sign-in'.
+				      mConnectionResult = result;
+				      updateButtons(false /* isSignedIn */);
+					    try {
+						      mIntentInProgress = true;
+						      result.startResolutionForResult(this, RC_SIGN_IN);
+						    } catch (SendIntentException e) {
+						    	  Log.i("onConnectionFailed",e.getMessage().toString());
+						      // The intent was canceled before it was sent.  Return to the default
+						      // state and attempt to connect to get an updated ConnectionResult.
+						      mIntentInProgress = false;
+						      mGoogleApiClient.connect();
+						    }
+
+				      if (mSignInClicked) {
+				        // The user has already clicked 'sign-in' so we attempt to resolve all
+				        // errors until the user is signed in, or they cancel.
+				        resolveSignInError();
+				      }
+				    }
+				  }
+
+			  public void onConnectionSuspended(int cause) {
+				  mGoogleApiClient.connect();
+				}
+
+			  private void resolveSignInError() {
+				    if (mConnectionResult.hasResolution()) {
+				      try {
+				        mIntentInProgress = true;
+				        mConnectionResult.startResolutionForResult(this, RC_SIGN_IN);
+				      } catch (SendIntentException e) {
+				    	  Log.i("resolveSignInError",e.getMessage().toString());
+				        // The intent was canceled before it was sent.  Return to the default
+				        // state and attempt to connect to get an updated ConnectionResult.
+				        mIntentInProgress = false;
+				        mGoogleApiClient.connect();
+				      }
+				    }
+				  }
+
+			  private void updateButtons(boolean isSignedIn) {
+		        if (isSignedIn) {
+		            mSignInButton.setVisibility(View.INVISIBLE);
+		            mSignOutButton.setEnabled(true);
+		            mRevokeAccessButton.setEnabled(true);
+		        } else {
+		            if (mConnectionResult == null) {
+		                // Disable the sign-in button until onConnectionFailed is called with result.
+		                mSignInButton.setVisibility(View.INVISIBLE);
+		                mSignInStatus.setText(getString(R.string.loading_status));
+		            } else {
+		                // Enable the sign-in button since a connection result is available.
+		                mSignInButton.setVisibility(View.VISIBLE);
+		                mSignInStatus.setText(getString(R.string.signed_out_status));
+		            }
+
+		            mSignOutButton.setEnabled(false);
+		            mRevokeAccessButton.setEnabled(false);
+		        }
+		    }
+	  
+				@Override
+				public void onClick(View view) {
+					  if (view.getId() == R.id.sign_in_button
+					    && !mGoogleApiClient.isConnecting()) {
+					    mSignInClicked = true;
+					    resolveSignInError();
+					  }
+					  
+				        switch(view.getId()) {
+			            case R.id.sign_in_button:
+			                int available = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+			                if (available != ConnectionResult.SUCCESS) {
+			                    //showDialog(DIALOG_GET_GOOGLE_PLAY_SERVICES);
+			                    return;
+			                }
+
+			                try {
+			                    mSignInStatus.setText(getString(R.string.signing_in_status));
+			                    mConnectionResult.startResolutionForResult(this, REQUEST_CODE_SIGN_IN);
+			                } catch (IntentSender.SendIntentException e) {
+			                    // Fetch a new result to start.
+			                	mGoogleApiClient.connect();
+			                    updateButtons(false /* isSignedIn */);
+			                }
+			                break;
+			            case R.id.sign_out_button:
+			                if (mGoogleApiClient.isConnected()) {
+			                    Plus.AccountApi.clearDefaultAccount(mGoogleApiClient);
+			                    mGoogleApiClient.disconnect();
+			                    updateButtons(false);
+			                  }
+			                
+			                break;
+			            case R.id.revoke_access_button:
+			                if (mGoogleApiClient.isConnected()) {
+			                	Plus.AccountApi.revokeAccessAndDisconnect(mGoogleApiClient);
+			                	mGoogleApiClient.disconnect();
+			                    updateButtons(false /* isSignedIn */);
+			                }
+			                break;
+				        }
+			        
+
+					}
+
+				protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+				  if (requestCode == RC_SIGN_IN) {
+				    if (responseCode != RESULT_OK) {
+				      mSignInClicked = false;
+				    }
+
+				    mIntentInProgress = false;
+
+				    if (!mGoogleApiClient.isConnecting()) {
+				      mGoogleApiClient.connect();
+				    }
+				  }
+				}
+		
+			@Override
+			public void onResult(LoadPeopleResult peopleData) {
+			  if (peopleData.getStatus().getStatusCode() == CommonStatusCodes.SUCCESS) {
+			    PersonBuffer personBuffer = peopleData.getPersonBuffer();
+			    try {
+			      int count = personBuffer.getCount();
+			      for (int i = 0; i < count; i++) {
+			        Log.d(TAG, "Display name: " + personBuffer.get(i).getDisplayName());
+			      }
+			    } finally {
+			      personBuffer.close();
+			    }
+			  } else {
+			    Log.e(TAG, "Error requesting visible circles: " + peopleData.getStatus());
+			  }
+			}	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -31,4 +275,7 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
+
+
 }
